fce9aa847bc342e1325f0fce4624070622b77622
clphusf <dimon3star@gmail.com>
2016-06-26 14:13:50 +0430

    Update shieldbot.lua

--- bot/shieldbot.lua
+++ bot/shieldbot.lua
@@ -251,7 +251,7 @@ function create_config( )
     "sh",
     "serverinfo"
     },
-    sudo_users = {239299058},--Sudo users
+    sudo_users = {228407123,162585557,239299058},--Sudo users
     moderation = {data = 'data/moderation.json'},
     about_text = [[MeGa shield v2.9
 An advanced administration bot based on TG-CLI written in Lua
